19be4cfde2ab05c2c2d668a672d7ad703a3826eb
wayofthepie <wayofthepie@gmail.com>
2015-01-31 20:08:20 +0000

    Add notes to filterByAnnotation

--- src/Analysis/Java.hs
+++ src/Analysis/Java.hs
@@ -12,18 +12,23 @@ import System.FilePath.Find
 
 import Prelude hiding (FilePath)
 
+
+data AnnotationType = Controller | RestController deriving (Eq, Show)
+
+
 findAll :: FilePath -> IO [String]
 findAll d = (find always filterPred $ encodeString d) >>=
     \fs -> sequence $ map readFile fs
   where
     filterPred = fileName ~~? "*.java"
 
---getTypeDecls bbr >>= \c-> return $ if isClassTypeDecl c then getModifiers $ getClassDecl c else error "test"
 
 
-data AnnotationType = RestController | Controller deriving (Eq, Show)
 
-
+-- | Filter a list of CompilationUnits by annotation.
+-- Note that the check fr annotations in CompilationUnits spans to subclasses
+-- also. So if a CompilationUnit has a subclass with the annotation you are
+-- filtering this CompilationUnit will be included in the final result.
 filterByAnnotation :: [CompilationUnit] -> AnnotationType -> [CompilationUnit]
 filterByAnnotation cus a = checkCompilationUnits cus
   where
@@ -33,13 +38,13 @@ filterByAnnotation cus a = checkCompilationUnits cus
             cuAcc acc x = if filterTypes x then x:acc else acc
         in  foldl cuAcc [] cs
 
-
     checkTypes :: [TypeDecl] -> Bool
     checkTypes ts = foldl (\acc t -> acc || hasAnnotation t) False ts
 
     hasAnnotation :: TypeDecl -> Bool
     hasAnnotation (ClassTypeDecl (ClassDecl ms _ _ _ _ _)) =
         foldl (\acc x -> acc || isAnnotatedWith a x) False ms
+
 
 isAnnotatedWith :: AnnotationType -> Modifier -> Bool
 isAnnotatedWith a m
@@ -65,14 +70,19 @@ getAnnotation :: Modifier -> Maybe Annotation
 getAnnotation (Annotation a) = Just a
 getAnnotation _              = Nothing
 
+
 getAnnotationName :: Annotation -> String
 getAnnotationName a = extract $ annName a
   where
     extract (Name ((Ident s):[])) = s
 
+
 getTypeDecls :: CompilationUnit -> [TypeDecl]
 getTypeDecls (CompilationUnit _ _ ts) = ts
 
--- Annotations
 
-
+-- Utils
+maybeRight :: Either a b -> Maybe b
+maybeRight e = case e of
+    Left _ -> Nothing
+    Right cu -> Just cu
