0267af1ee68dcfbac9137adbb1fe074c8df3a787
wayofthepie <wayofthepie@gmail.com>
2015-01-31 22:13:08 +0000

    Add initial work on java rest api analysis
    This is not in a complete state and contains many issues.... Lots more
    work to do!

--- src/Analysis/Java.hs
+++ src/Analysis/Java.hs
@@ -26,7 +26,7 @@ findAll d = (find always filterPred $ encodeString d) >>=
 
 
 -- | Filter a list of CompilationUnits by annotation.
--- Note that the check fr annotations in CompilationUnits spans to subclasses
+-- Note that the check for annotations in CompilationUnits spans to subclasses
 -- also. So if a CompilationUnit has a subclass with the annotation you are
 -- filtering this CompilationUnit will be included in the final result.
 filterByAnnotation :: [CompilationUnit] -> AnnotationType -> [CompilationUnit]
@@ -52,8 +52,12 @@ filterByAnnotation cus a = checkCompilationUnits cus
     -- Something else ....
     hasAnnotation _ = False
 
+
+-- | Verifies whether one or more of the given Modifiers are annotated with
+-- the given annotation.
 checkModifiers :: [Modifier] -> AnnotationType -> Bool
 checkModifiers ms a = foldl (\acc x -> acc || isAnnotatedWith a x) False ms
+
 
 isAnnotatedWith :: AnnotationType -> Modifier -> Bool
 isAnnotatedWith a m
@@ -88,6 +92,107 @@ getAnnotationName a = extract $ annName a
 
 getTypeDecls :: CompilationUnit -> [TypeDecl]
 getTypeDecls (CompilationUnit _ _ ts) = ts
+
+
+-- | Extract information.
+data JavaClassMetaInfo = JavaClassMetaInfo
+    { className :: String
+    , apiState  :: ApiState
+    } deriving (Eq, Show)
+
+-- | Currently I just want to focus on RESTful API's and only their method
+-- signatures.
+data ApiState = ApiState
+    { methodSigs :: [[MethodInfo]]
+    } deriving (Eq, Show)
+
+
+data MethodInfo = MethodInfo
+    { modifiers :: [Modifier]
+    , typeParams:: [TypeParam]
+    , generics  :: Maybe Type
+    , ident     :: Ident
+    , params    :: [FormalParam]
+    , exceptoins:: [ExceptionType]
+    } deriving (Eq, Show)
+
+gather :: [CompilationUnit] -> [JavaClassMetaInfo]
+gather cus = map gather' cus
+
+-- |
+-- TODO : Optimize this, way too many list passes ...
+-- TODO : Java Classes can have nested classes, JavaClassMetaInfo
+-- needs to relect this and be updated with nested Maybe [JavaClassMetaInfo]
+-- or something similar and this method must then be updated to
+-- parse data into that structure.
+gather' :: CompilationUnit -> JavaClassMetaInfo
+gather' cu = do
+    JavaClassMetaInfo "test" $ ApiState  $ extractMethodInfo $
+        filterMethodDecl $ extractMemberDecl $ filterMemberDecl $
+            extractDecls  $ extractClassBodies $ filterClassTypeDecl $ getTypeDecls cu
+
+-------------------------------------------------------------------------------
+-- | Filtering
+
+filterClassTypeDecl :: [TypeDecl] -> [TypeDecl]
+filterClassTypeDecl = filter isClassTypeDecl
+
+extractClassBodies :: [TypeDecl] -> [ClassBody]
+extractClassBodies = map ( xClassBody . xClassDecl )
+
+extractDecls :: [ClassBody] -> [[Decl]]
+extractDecls = map xDecls
+
+filterMemberDecl :: [[Decl]] -> [[Decl]]
+filterMemberDecl = map (filter memberDeclPred)
+
+extractMemberDecl :: [[Decl]] -> [[MemberDecl]]
+extractMemberDecl = map xMemberDecls
+
+filterMethodDecl :: [[MemberDecl]] -> [[MemberDecl]]
+filterMethodDecl = map (filter methodDeclPred)
+
+extractMethodInfo :: [[MemberDecl]] -> [[MethodInfo]]
+extractMethodInfo = map xMethodInfo
+
+-- Extraction
+-- | TODO : Functionality for Enums must be added, currently
+-- most below functions are partial and only deal with classes
+
+xClassDecl :: TypeDecl -> ClassDecl
+xClassDecl (ClassTypeDecl c) = c
+xClassDecl _ = error "undefined"
+
+xClassBody :: ClassDecl -> ClassBody
+xClassBody (ClassDecl _ _ _ _ _ cb) = cb
+xClassBody (EnumDecl _ _ _ _) =
+    error "Functionality for Enums not in place yet."
+
+xDecls :: ClassBody -> [Decl]
+xDecls (ClassBody dcls) = dcls
+
+xMemberDecls :: [Decl] -> [MemberDecl]
+xMemberDecls = map xMemberDecl
+
+xMemberDecl :: Decl -> MemberDecl
+xMemberDecl (MemberDecl m) = m
+xMemberDecl _ = error "undefined"
+
+xMethodInfo :: [MemberDecl] -> [MethodInfo]
+xMethodInfo = map xMethodInfo'
+
+xMethodInfo' :: MemberDecl -> MethodInfo
+xMethodInfo' (MethodDecl a b c d e f _) = MethodInfo a b c d e f
+xMethodInfo' _ = error "undefined"
+
+
+memberDeclPred :: Decl -> Bool
+memberDeclPred (MemberDecl _) = True
+memberDeclPred _                 = False
+
+methodDeclPred :: MemberDecl -> Bool
+methodDeclPred (MethodDecl _ _ _ _ _ _ _) = True
+methodDeclPred _                          = False
 
 
 -- Utils
